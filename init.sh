9e47166eee618541b31c84e2d7520efe77a3c2ca
Sean Lee <freshdried@gmail.com>
2023-12-27 02:30:59 -0300

    wip.

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# cd to the directory of the script
+(cd "$(dirname "$0")" || exit; pwd -P)
+
 # Run to generate a fresh workspace
-rm -rf ~/.ssh
-ln -s /workspace/.ssh ~/
+# rm -rf ~/.ssh
+# ln -s /workspace/.ssh ~/
 
 pip install --upgrade pip
 pip install poetry
