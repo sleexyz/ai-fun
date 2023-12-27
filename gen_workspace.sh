1cc6cea9603dcfc2de17d8cdb229f7315e1b3d0b
Sean Lee <freshdried@gmail.com>
2023-12-27 05:05:53 +0000

    wip.

--- gen_workspace.sh
+++ gen_workspace.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+set -e
+
 # Run to generate a fresh workspace
-cp -r ~/.cursor-server /workspace
-rm -rf ~/.cursor-server
-ln -s /workspace/.cursor-server ~/
+if [ -d "/workspace/.ssh" ]; then
+    echo ".ssh already exists"
+    exit 1
+else
+    cp -r ~/.ssh /workspace
+    rm -rf ~/.ssh
+    ln -s /workspace/.ssh ~/
+fi
--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Run to generate a fresh workspace
+rm -rf ~/.ssh
+ln -s /workspace/.ssh ~/
+
 pip install --upgrade pip
 pip install poetry
 poetry install --no-root
